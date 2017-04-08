9e8078aee583906e96365ac17dc0229925e2eb97
Alexey Kuleshevich <alexey@fpcomplete.com>
2017-04-08 17:35:32 +0300

    seprarated tests for manifest from delayed

--- tests/Data/Array/MassivSpec.hs
+++ tests/Data/Array/MassivSpec.hs
@@ -6,6 +6,7 @@ module Data.Array.MassivSpec (spec) where
 import           Data.Array.Massiv
 import           Data.Array.Massiv.DelayedSpec()
 import           Data.Array.Massiv.CommonSpec (ArrIx (..))
+import           Data.Array.Massiv.ManifestSpec ()
 import           Test.Hspec
 import           Test.QuickCheck
 
@@ -30,20 +31,37 @@ prop_ExtractAppend _ thresh dim (ArrIx arr ix) =
     dim' = (dim `mod` (rank ix)) + 1
 
 
-massivSpecN
-  :: (Eq e, Unbox e, Shape r ix e, Load r ix, Arbitrary (ArrIx r ix e))
-  => proxy (r, ix, e) -> Spec
-massivSpecN r = do
-  describe "ExtractAppend" $ do
-    it "ExtractAppend" $ property $ prop_ExtractAppend r 100000
+massivSpecNDelayed
+  :: (Eq e, Shape D ix e, Arbitrary (ArrIx D ix e))
+  => proxy (D, ix, e) -> Spec
+massivSpecNDelayed r = do
+  it "ExtractAppend" $ property $ prop_ExtractAppend r 100000
+  it "ExtractAppend" $ property $ prop_ExtractAppend r 100000
+
+massivSpecNManifest
+  :: (Eq e, Shape M ix e, Arbitrary (ArrIx M ix e))
+  => proxy (M, ix, e) -> Spec
+massivSpecNManifest r = do
+  it "ExtractAppend" $ property $ prop_ExtractAppend r 100000
+
 
 spec :: Spec
 spec = do
-  describe "DIM1" $ do
-    massivSpecN (Nothing :: Maybe (D, DIM1, Int))
-  describe "DIM2" $ do
-    massivSpecN (Nothing :: Maybe (D, DIM2, Int))
-  describe "DIM3" $ do
-    massivSpecN (Nothing :: Maybe (D, DIM3, Int))
-  describe "DIM4" $ do
-    massivSpecN (Nothing :: Maybe (D, DIM4, Int))
+  describe "Delayed" $ do
+    describe "DIM1" $ do
+      massivSpecNDelayed (Nothing :: Maybe (D, DIM1, Int))
+    describe "DIM2 - Delayed" $ do
+      massivSpecNDelayed (Nothing :: Maybe (D, DIM2, Int))
+    describe "DIM3 - Delayed" $ do
+      massivSpecNDelayed (Nothing :: Maybe (D, DIM3, Int))
+    describe "DIM4 - Delayed" $ do
+      massivSpecNDelayed (Nothing :: Maybe (D, DIM4, Int))
+  describe "Manifest" $ do
+    describe "DIM1 - Manifest" $ do
+      massivSpecNManifest (Nothing :: Maybe (M, DIM1, Int))
+    describe "DIM2 - Manifest" $ do
+      massivSpecNManifest (Nothing :: Maybe (M, DIM2, Int))
+    describe "DIM3 - Manifest" $ do
+      massivSpecNManifest (Nothing :: Maybe (M, DIM3, Int))
+    describe "DIM4 - Manifest" $ do
+      massivSpecNManifest (Nothing :: Maybe (M, DIM4, Int))
