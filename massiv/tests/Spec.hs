8f9d9904e79a38c2752e1e676c791d1e01ddecc5
Alexey Kuleshevich <alexey@fpcomplete.com>
2017-12-24 21:03:55 +0300

    added delayed stencil spec

--- massiv/tests/Spec.hs
+++ massiv/tests/Spec.hs
@@ -5,6 +5,8 @@ module Main where
 import           Test.Hspec
 import Data.Massiv.Core.SchedulerSpec as Scheduler
 import Data.Massiv.Core.IndexSpec as Index
+import Data.Massiv.Array.DelayedSpec as Delayed
+import Data.Massiv.Array.StencilSpec as Stencil
 
 
 -- | Main entry point. Returns ExitFailure if a test fails.
@@ -12,6 +14,8 @@ main :: IO ()
 main = hspec $ do
   Scheduler.spec
   Index.spec
+  Delayed.spec
+  Stencil.spec
 
 -- -- | Test suite.
 -- spec :: Spec
