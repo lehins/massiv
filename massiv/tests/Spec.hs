98d61eac17fdd3cbfb1f71b9a9929e26fc7c4324
Alexey Kuleshevich <alexey@fpcomplete.com>
2017-12-24 17:24:11 +0300

    turned on index spec

--- massiv/tests/Spec.hs
+++ massiv/tests/Spec.hs
@@ -3,11 +3,15 @@
 module Main where
 
 import           Test.Hspec
-import Data.Massiv.Core.SchedulerSpec
+import Data.Massiv.Core.SchedulerSpec as Scheduler
+import Data.Massiv.Core.IndexSpec as Index
+
 
 -- | Main entry point. Returns ExitFailure if a test fails.
 main :: IO ()
-main = hspec spec
+main = hspec $ do
+  Scheduler.spec
+  Index.spec
 
 -- -- | Test suite.
 -- spec :: Spec
