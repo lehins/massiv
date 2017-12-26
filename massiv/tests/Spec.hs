34b0b798830548ba16ec4660efd9a91f749cf65d
Alexey Kuleshevich <alexey@fpcomplete.com>
2017-12-26 13:38:54 +0300

    attempt to fix appveyor builds. brough back all the specs, excpet the stencil one

--- massiv/tests/Spec.hs
+++ massiv/tests/Spec.hs
@@ -1,21 +1,21 @@
---{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
 
-module Main where
+-- module Main where
 
-import           Test.Hspec
-import Data.Massiv.Core.SchedulerSpec as Scheduler
-import Data.Massiv.Core.IndexSpec as Index
-import Data.Massiv.Array.DelayedSpec as Delayed
-import Data.Massiv.Array.StencilSpec as Stencil
+-- import           Test.Hspec
+-- import Data.Massiv.Core.SchedulerSpec as Scheduler
+-- import Data.Massiv.Core.IndexSpec as Index
+-- import Data.Massiv.Array.DelayedSpec as Delayed
+-- import Data.Massiv.Array.StencilSpec as Stencil
 
 
--- | Main entry point. Returns ExitFailure if a test fails.
-main :: IO ()
-main = hspec $ do
-  Scheduler.spec
-  Index.spec
-  Delayed.spec
-  --Stencil.spec
+-- -- | Main entry point. Returns ExitFailure if a test fails.
+-- main :: IO ()
+-- main = hspec $ do
+--   Scheduler.spec
+--   Index.spec
+--   Delayed.spec
+--   --Stencil.spec
 
 -- -- | Test suite.
 -- spec :: Spec
