d285c1d5db82caf2468b6630eef4e03262b86c24
Alexey Kuleshevich <alexey@fpcomplete.com>
2017-12-26 13:54:58 +0300

    removed denug info from appveyor, set linebuffering on stdout in tests

--- massiv/tests/Spec.hs
+++ massiv/tests/Spec.hs
@@ -1,22 +1,24 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+--{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
 
--- module Main where
+module Main where
 
--- import           Test.Hspec
--- import Data.Massiv.Core.SchedulerSpec as Scheduler
--- import Data.Massiv.Core.IndexSpec as Index
--- import Data.Massiv.Array.DelayedSpec as Delayed
--- import Data.Massiv.Array.StencilSpec as Stencil
+import           Data.Massiv.Array.DelayedSpec  as Delayed
+import           Data.Massiv.Array.StencilSpec  as Stencil
+import           Data.Massiv.Core.IndexSpec     as Index
+import           Data.Massiv.Core.SchedulerSpec as Scheduler
+import           System.IO                      (BufferMode (LineBuffering),
+                                                 hSetBuffering, stdout)
+import           Test.Hspec
 
 
--- -- | Main entry point. Returns ExitFailure if a test fails.
--- main :: IO ()
--- main = hspec $ do
---   Scheduler.spec
---   Index.spec
---   Delayed.spec
---   --Stencil.spec
+-- | Main entry point. Returns ExitFailure if a test fails.
+main :: IO ()
+main = hSetBuffering stdout LineBuffering >> hspec $ do
+  Scheduler.spec
+  Index.spec
+  Delayed.spec
+  Stencil.spec
 
 -- -- | Test suite.
 -- spec :: Spec
--- spec = 
+-- spec =
