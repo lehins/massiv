e6b6a319acde209fc5e92daca2f84240e6fd8be5
Nicolas Mattia <nicolas@nmattia.com>
2018-06-09 21:50:41 +0200

    Add nix shell

---
+++ shell.nix
@@ -0,0 +1,18 @@
+let
+  pkgs = import <nixpkgs> {};
+  # Wrapped stack executable that uses the nix-provided GHC
+  stack = pkgs.stdenv.mkDerivation {
+      name = "stack-system-ghc";
+      builder = pkgs.writeScript "stack" ''
+        source $stdenv/setup
+        mkdir -p $out/bin
+        makeWrapper ${pkgs.stack}/bin/stack $out/bin/stack \
+          --add-flags --system-ghc
+      '';
+      buildInputs = [ pkgs.makeWrapper ];
+    };
+in pkgs.mkShell
+{
+  buildInputs = [ stack pkgs.haskell.compiler.ghc822 pkgs.gmp ];
+
+}
