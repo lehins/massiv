777a20e3a5296169431e650fdc734eab575067e2
Alexey Kuleshevich <lehins@yandex.ru>
2018-09-15 20:39:30 +0300

    Started to move all benchmarks unrelated to massiv to massiv-benchmarks repository

---
+++ massiv-bench/bench/Sobel.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE BangPatterns     #-}
+module Main where
+
+import           Criterion.Main
+import           Data.Massiv.Array as A
+import           Data.Massiv.Bench as A
+import           Data.Massiv.Bench.Sobel as A
+import           Prelude           as P
+
+
+
+main :: IO ()
+main = do
+  let !sz = 1600 :. 1200
+  defaultMain
+    [ bgroup
+        "Sobel"
+        [ env (return (arrRLightIx2 U Seq sz)) $ \arr ->
+            bgroup
+              "Seq"
+              [ bench "Horizontal - Massiv" $ whnf (computeAs U . A.mapStencil Edge sobelX) arr
+              , bench "Vertical - Massiv" $ whnf (computeAs U . A.mapStencil Edge sobelY) arr
+              , bench "Operator - Massiv" $ whnf (computeAs U . A.mapStencil Edge sobelOperator) arr
+              ]
+        , env (return (arrRLightIx2 U Par sz)) $ \arr ->
+            bgroup
+              "Par"
+              [ bench "Horizontal - Massiv" $ whnf (computeAs U . A.mapStencil Edge sobelX) arr
+              , bench "Vertical - Massiv" $ whnf (computeAs U . A.mapStencil Edge sobelY) arr
+              , bench "Operator - Massiv" $ whnf (computeAs U . A.mapStencil Edge sobelOperator) arr
+              ]
+        ]
+    ]
