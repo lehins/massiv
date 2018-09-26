7e87e2ae43a7920c2a1c77f979a8568351f07cee
Alexey Kuleshevich <lehins@yandex.ru>
2018-09-26 20:36:56 +0300

    basic scheduler benchmark

---
+++ massiv-bench/bench/Scheduler.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE BangPatterns #-}
+module Main where
+
+import Control.Monad
+import           Criterion.Main
+import           Data.Massiv.Bench          as A
+import           Data.Massiv.Core.Index
+import           Data.Massiv.Core.Scheduler as A
+import           Prelude                    as P
+
+
+mapConcurrently :: Foldable t => (a -> IO b) -> t a -> IO [b]
+mapConcurrently f ls = withScheduler' [] $ \s ->
+  mapM_ (scheduleWork s . f) ls
+
+main :: IO ()
+main = do
+  let !sz@(_ :. k) = 600 :. 200
+  defaultMain
+    [ env (return (totalElem sz)) $ \n ->
+        bgroup
+          "Map"
+          [ bench "mapConcurrently (lightFuncIx1)" $
+            whnfIO (mapConcurrently (\i -> return $! lightFuncIx1 k i) [0 .. n - 1])
+          , bench "mapConcurrently (succ)" $
+            whnfIO (mapConcurrently (\i -> return $! succ i) [0 .. n - 1])
+          ]
+    ]
