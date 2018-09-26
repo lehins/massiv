4b15504bfb9bb2856f48d2453937cce03406b6db
Alexey Kuleshevich <lehins@yandex.ru>
2018-09-26 05:46:53 +0300

    Alternative Matrix multiplicatin benchmark

--- massiv-bench/bench/Mult.hs
+++ massiv-bench/bench/Mult.hs
@@ -3,9 +3,26 @@ module Main where
 
 import           Criterion.Main
 import           Data.Massiv.Array as A
+import           Data.Massiv.Array.Unsafe as A
 import           Data.Massiv.Bench as A
 import           Prelude           as P
 
+
+
+multArrs :: (Unbox e, Num e) => Array U Ix2 e -> Array U Ix2 e -> Array U Ix2 e
+multArrs arr1 arr2
+  | n1 /= m2 =
+    error $
+    "(|*|): Inner array dimensions must agree, but received: " ++
+    show (size arr1) ++ " and " ++ show (size arr2)
+  | otherwise = compute $
+    makeArrayR D (getComp arr1 <> getComp arr2) (m1 :. n2) $ \(i :. j) ->
+      A.foldlS (+) 0 (A.zipWith (*) (unsafeOuterSlice arr1 i) (unsafeOuterSlice arr2' j))
+  where
+    (m1 :. n1) = size arr1
+    (m2 :. n2) = size arr2
+    arr2' = computeAs U $ transpose arr2
+{-# INLINE multArrs #-}
 
 
 main :: IO ()
@@ -18,9 +35,13 @@ main = do
           "Mult"
           [ bgroup
               "Seq"
-              [bench "(|*|)" $ whnf (A.computeAs U . (|*|) (setComp Seq arr)) arr']
+              [ bench "(|*|)" $ whnf (A.computeAs U . (|*|) (setComp Seq arr)) arr'
+              , bench "mmultArrs" $ whnf (multArrs (setComp Seq arr)) arr'
+              ]
           , bgroup
               "Par"
-              [bench "(|*|)" $ whnf (A.computeAs U . (|*|) (setComp Par arr)) arr']
+              [ bench "(|*|)" $ whnf (A.computeAs U . (|*|) (setComp Par arr)) arr'
+              , bench "mmultArrs" $ whnf (multArrs (setComp Par arr)) arr'
+              ]
           ]
     ]
