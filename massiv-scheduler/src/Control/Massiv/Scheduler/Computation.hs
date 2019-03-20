33fccd6f2ebcd465c5e3249f41799e6c51d20a70
Alexey Kuleshevich <alexey@kuleshevi.ch>
2019-03-20 22:53:24 +0300

    Fix up monoid instance for Comp

--- massiv-scheduler/src/Control/Massiv/Scheduler/Computation.hs
+++ massiv-scheduler/src/Control/Massiv/Scheduler/Computation.hs
@@ -19,24 +19,19 @@ import Data.Semigroup
 #endif
 import Data.Word
 
--- | Computation type to use when scheduling work.
+-- | Computation strategy to use when scheduling work.
 data Comp
   = Seq -- ^ Sequential computation
   | ParOn ![Int]
-  -- ^ Use `Par` instead to use your CPU to the fullest. Also don't forget to compile
-  -- the program with @-threaded@ flag.
-  --
-  -- Parallel computation with a list of capabilities to run computation
-  -- on. Specifying an empty list (@ParOn []@) or using `Par` will result in
-  -- utilization of all available capabilities, which are set at runtime by
-  -- @+RTS -Nx@ or at compile time by GHC flag @-with-rtsopts=-Nx@,
-  -- where @x@ is the number of capabilities. Ommiting @x@ in above flags
-  -- defaults to number available cores.
+  -- ^ Schedule workers to run on specific capabilities. Specifying an empty list @`ParOn` []@ or
+  -- using `Par` will result in utilization of all available capabilities.
   | ParN {-# UNPACK #-} !Word16
-  -- ^ Specify the number of workers that will be handling all the jobs.
+  -- ^ Specify the number of workers that will be handling all the jobs. Difference from `ParOn` is
+  -- that workers can jump between cores. Using @`ParN` 0@ will result in using all available
+  -- capabilities.
   deriving Eq
 
--- | Parallel computation using all available cores.
+-- | Parallel computation using all available cores. Same as @`ParOn` []@
 pattern Par :: Comp
 pattern Par <- ParOn [] where
         Par =  ParOn []
@@ -79,7 +74,9 @@ joinComp x y =
         Seq      -> x
         Par      -> Par
         ParOn ys -> ParOn (xs ++ ys)
+        ParN 0   -> ParN 0
         ParN n2  -> ParN (max (fromIntegral (length xs)) n2)
+    ParN 0 -> ParN 0
     ParN n1 ->
       case y of
         Seq      -> x
