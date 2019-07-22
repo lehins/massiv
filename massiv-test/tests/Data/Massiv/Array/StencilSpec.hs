c348a8b63f8c5fdad6e96e437a42d8d3d645dcd4
Alexey Kuleshevich <alexey@kuleshevi.ch>
2019-07-23 01:42:08 +0300

    Turn off tests for ghc<=8.2 due to some ghc stack overflow bug

--- massiv-test/tests/Data/Massiv/Array/StencilSpec.hs
+++ massiv-test/tests/Data/Massiv/Array/StencilSpec.hs
@@ -19,22 +19,22 @@ import Test.Massiv.Core
 -- {-# INLINE sum3x3Stencil #-}
 
 
--- singletonStencil :: (Index ix) => (Int -> Int) -> Stencil ix Int Int
--- singletonStencil f =
---   makeStencil oneSz zeroIndex $ \ get -> fmap f (get zeroIndex)
--- {-# INLINE singletonStencil #-}
+singletonStencil :: (Index ix) => (Int -> Int) -> Stencil ix Int Int
+singletonStencil f =
+  makeStencil oneSz zeroIndex $ \ get -> fmap f (get zeroIndex)
+{-# INLINE singletonStencil #-}
 
 
--- prop_MapSingletonStencil :: (Load DW ix Int, Manifest U ix Int) =>
---                             Proxy ix -> Fun Int Int -> Border Int -> ArrNE U ix Int -> Bool
--- prop_MapSingletonStencil _ f b (ArrNE arr) =
---   computeAs U (mapStencil b (singletonStencil (apply f)) arr) == computeAs U (A.map (apply f) arr)
+prop_MapSingletonStencil :: (Load DW ix Int, Manifest U ix Int) =>
+                            Proxy ix -> Fun Int Int -> Border Int -> ArrNE U ix Int -> Bool
+prop_MapSingletonStencil _ f b (ArrNE arr) =
+  computeAs U (mapStencil b (singletonStencil (apply f)) arr) == computeAs U (A.map (apply f) arr)
 
--- prop_MapSingletonStencilWithStride :: (StrideLoad DW ix Int, Manifest U ix Int) =>
---                                       Proxy ix -> Fun Int Int -> Border Int -> ArrNE U ix Int -> Bool
--- prop_MapSingletonStencilWithStride _ f b (ArrNE arr) =
---   computeWithStride oneStride (mapStencil b (singletonStencil (apply f)) arr) ==
---   computeAs U (A.map (apply f) arr)
+prop_MapSingletonStencilWithStride :: (StrideLoad DW ix Int, Manifest U ix Int) =>
+                                      Proxy ix -> Fun Int Int -> Border Int -> ArrNE U ix Int -> Bool
+prop_MapSingletonStencilWithStride _ f b (ArrNE arr) =
+  computeWithStride oneStride (mapStencil b (singletonStencil (apply f)) arr) ==
+  computeAs U (A.map (apply f) arr)
 
 -- Tests out of bounds stencil indexing
 prop_DangerousStencil ::
@@ -47,17 +47,16 @@ prop_DangerousStencil _ (NonZero s) (DimIx r) (SzIx sz ix) =
 
 stencilSpec :: Spec
 stencilSpec = do
-  -- Deadlocks?????? lts-12.26
-  -- describe "MapSingletonStencil" $ do
-  --   it "Ix1" $ property $ prop_MapSingletonStencil (Proxy :: Proxy Ix1)
-  --   it "Ix2" $ property $ prop_MapSingletonStencil (Proxy :: Proxy Ix2)
-  --   it "Ix3" $ property $ prop_MapSingletonStencil (Proxy :: Proxy Ix3)
-  --   it "Ix4" $ property $ prop_MapSingletonStencil (Proxy :: Proxy Ix4)
-  --   it "Ix2T" $ property $ prop_MapSingletonStencil (Proxy :: Proxy Ix2T)
-  -- describe "MapSingletonStencilWithStride" $ do
-  --   it "Ix1" $ property $ prop_MapSingletonStencilWithStride (Proxy :: Proxy Ix1)
-  --   it "Ix2" $ property $ prop_MapSingletonStencilWithStride (Proxy :: Proxy Ix2)
-  --   it "Ix3" $ property $ prop_MapSingletonStencilWithStride (Proxy :: Proxy Ix3)
+  describe "MapSingletonStencil" $ do
+    it "Ix1" $ property $ prop_MapSingletonStencil (Proxy :: Proxy Ix1)
+    it "Ix2" $ property $ prop_MapSingletonStencil (Proxy :: Proxy Ix2)
+    it "Ix3" $ property $ prop_MapSingletonStencil (Proxy :: Proxy Ix3)
+    it "Ix4" $ property $ prop_MapSingletonStencil (Proxy :: Proxy Ix4)
+    it "Ix2T" $ property $ prop_MapSingletonStencil (Proxy :: Proxy Ix2T)
+  describe "MapSingletonStencilWithStride" $ do
+    it "Ix1" $ property $ prop_MapSingletonStencilWithStride (Proxy :: Proxy Ix1)
+    it "Ix2" $ property $ prop_MapSingletonStencilWithStride (Proxy :: Proxy Ix2)
+    it "Ix3" $ property $ prop_MapSingletonStencilWithStride (Proxy :: Proxy Ix3)
   describe "DangerousStencil" $ do
     it "Ix1" $ property $ prop_DangerousStencil (Proxy :: Proxy Ix1)
     it "Ix2" $ property $ prop_DangerousStencil (Proxy :: Proxy Ix2)
@@ -149,40 +148,39 @@ stencilConvolution = do
 
 spec :: Spec
 spec = do
-  -- describe "Stencil" $ do
-  --   stencilSpec
-  --   let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] :: Array U Ix2 Int
-  --   describe "Unit tests Ix2" $ do
-  --     it "Direction Left" $
-  --       stencilDirection (0 :. 1) arr `shouldBe` [[2, 3, 0], [5, 6, 0], [8, 9, 0]]
-  --     it "Direction Right" $
-  --       stencilDirection (0 :. -1) arr `shouldBe` [[0, 1, 2], [0, 4, 5], [0, 7, 8]]
-  --     it "Direction Down" $
-  --       stencilDirection (1 :. 0) arr `shouldBe` [[4, 5, 6], [7, 8, 9], [0, 0, 0]]
-  --     it "Direction Up" $
-  --       stencilDirection (-1 :. 0) arr `shouldBe` [[0, 0, 0], [1, 2, 3], [4, 5, 6]]
-  --     it "Direction Left/Top Corner" $
-  --       stencilCorners (0 :. 0) (2 :. 2) arr `shouldBe` [[9, 0, 0], [0, 0, 0], [0, 0, 0]]
-  --     it "Direction Right/Top Corner" $
-  --       stencilCorners (0 :. 2) (2 :. -2) arr `shouldBe` [[0, 0, 7], [0, 0, 0], [0, 0, 0]]
-  --     it "Direction Right/Bottom Corner" $
-  --       stencilCorners (2 :. 2) (-2 :. -2) arr `shouldBe` [[0, 0, 0], [0, 0, 0], [0, 0, 1]]
-  --     it "Direction Left/Bottom Corner" $
-  --       stencilCorners (2 :. 0) (-2 :. 2) arr `shouldBe` [[0, 0, 0], [0, 0, 0], [3, 0, 0]]
-  --   describe "mapStencil with stride" $ do
-  --     let kernel = [[-1, 0, 1], [0, 1, 0], [-1, 0, 1]] :: Array U Ix2 Int
-  --         stencil = makeConvolutionStencilFromKernel kernel
-  --         stride = Stride 2
-  --     it "map stencil with stride on small array" $
-  --       let strideArr = mapStencil (Fill 0) stencil arr
-  --        in computeWithStrideAs U stride strideArr `shouldBe` [[-4, 8], [2, 14]]
-  --     it "map stencil with stride on larger array" $
-  --       let largeArr = makeArrayR U Seq (Sz 5) (succ . toLinearIndex (Sz 5))
-  --           strideArr = mapStencil (Fill 0) stencil largeArr
-  --        in computeWithStrideAs U stride strideArr `shouldBe`
-  --           [[-6, 1, 14], [-13, 9, 43], [4, 21, 44]]
-  -- stencilConvolution
-  pure ()
+  describe "Stencil" $ do
+    stencilSpec
+    let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] :: Array U Ix2 Int
+    describe "Unit tests Ix2" $ do
+      it "Direction Left" $
+        stencilDirection (0 :. 1) arr `shouldBe` [[2, 3, 0], [5, 6, 0], [8, 9, 0]]
+      it "Direction Right" $
+        stencilDirection (0 :. -1) arr `shouldBe` [[0, 1, 2], [0, 4, 5], [0, 7, 8]]
+      it "Direction Down" $
+        stencilDirection (1 :. 0) arr `shouldBe` [[4, 5, 6], [7, 8, 9], [0, 0, 0]]
+      it "Direction Up" $
+        stencilDirection (-1 :. 0) arr `shouldBe` [[0, 0, 0], [1, 2, 3], [4, 5, 6]]
+      it "Direction Left/Top Corner" $
+        stencilCorners (0 :. 0) (2 :. 2) arr `shouldBe` [[9, 0, 0], [0, 0, 0], [0, 0, 0]]
+      it "Direction Right/Top Corner" $
+        stencilCorners (0 :. 2) (2 :. -2) arr `shouldBe` [[0, 0, 7], [0, 0, 0], [0, 0, 0]]
+      it "Direction Right/Bottom Corner" $
+        stencilCorners (2 :. 2) (-2 :. -2) arr `shouldBe` [[0, 0, 0], [0, 0, 0], [0, 0, 1]]
+      it "Direction Left/Bottom Corner" $
+        stencilCorners (2 :. 0) (-2 :. 2) arr `shouldBe` [[0, 0, 0], [0, 0, 0], [3, 0, 0]]
+    describe "mapStencil with stride" $ do
+      let kernel = [[-1, 0, 1], [0, 1, 0], [-1, 0, 1]] :: Array U Ix2 Int
+          stencil = makeConvolutionStencilFromKernel kernel
+          stride = Stride 2
+      it "map stencil with stride on small array" $
+        let strideArr = mapStencil (Fill 0) stencil arr
+         in computeWithStrideAs U stride strideArr `shouldBe` [[-4, 8], [2, 14]]
+      it "map stencil with stride on larger array" $
+        let largeArr = makeArrayR U Seq (Sz 5) (succ . toLinearIndex (Sz 5))
+            strideArr = mapStencil (Fill 0) stencil largeArr
+         in computeWithStrideAs U stride strideArr `shouldBe`
+            [[-6, 1, 14], [-13, 9, 43], [4, 21, 44]]
+  stencilConvolution
 
 sobelX :: Num e => (Ix2 -> e -> e -> e) -> e -> e
 sobelX f = f (-1 :. -1) (-1) . f (-1 :. 1) 1 .
