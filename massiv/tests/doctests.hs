ad4f50a19bf639cba0bc8126c8f3df78769dec10
Alexey Kuleshevich <alexey@kuleshevi.ch>
2021-03-15 01:30:32 +0300

    Fix doctests for newer ghc

--- massiv/tests/doctests.hs
+++ massiv/tests/doctests.hs
@@ -1,6 +1,17 @@
+{-# LANGUAGE CPP #-}
 module Main where
+
+#if __GLASGOW_HASKELL__ >= 802 && __GLASGOW_HASKELL__ < 810
 
 import Test.DocTest (doctest)
 
 main :: IO ()
 main = doctest ["-Iinclude","src"]
+
+#else
+
+-- TODO: fix doctest support
+main :: IO ()
+main = putStrLn "\nDoctests are not supported for older ghc version\n"
+
+#endif
