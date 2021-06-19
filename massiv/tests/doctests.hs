a65a2cac45ca16c29c5fc283cf39e05f4a8ea8ac
Alexey Kuleshevich <alexey@kuleshevi.ch>
2021-06-19 22:22:56 +0300

    Disable doctests for ghc-8.10

--- massiv/tests/doctests.hs
+++ massiv/tests/doctests.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE CPP #-}
 module Main where
 
-#if __GLASGOW_HASKELL__ >= 802
+#if __GLASGOW_HASKELL__ >= 802 && __GLASGOW_HASKELL__ != 810
 
 import Test.DocTest (doctest)
 
@@ -12,6 +12,6 @@ main = doctest ["-Iinclude","src"]
 
 -- TODO: fix doctest support
 main :: IO ()
-main = putStrLn "\nDoctests are not supported for older ghc version\n"
+main = putStrLn "\nDoctests are not supported for ghc version 8.2 and prior as well as 8.10\n"
 
 #endif
